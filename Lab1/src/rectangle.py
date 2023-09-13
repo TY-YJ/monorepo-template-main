61deb189ca099510917b98042a9ddd43580cf2da
Taeyoon Kim <taeyoonkim@Taeyoons-MacBook-Pro.local>
2023-09-13 16:53:05 -0700

    Refactored rectangle class

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -20,23 +20,40 @@ class Shape(ABC):
 
 class Rectangle(Shape):
     def __init__(self, width, height) -> None:
-        self._width = width
-        self._height = height
+        self.__width = width
+        self.__height = height
 
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        self.__width = x
+        self.__height = y
+
+    def get_width(self):
+        return self.__width
+    
+    def get_height(self):
+        return self.__height
+
+    def set_width(self, width):
+        self.__width = width
+    
+    def set_height(self, height):
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(2, 3)
+    print("width:", rect.get_width())
+    print("height:", rect.get_height())
 
     # Call a member function
     rect.set_values(3, 4)
+    
+    print("width:", rect.get_width())
+    print("height:", rect.get_height())
 
     # Print out the area function
     print("area:", rect.area())
