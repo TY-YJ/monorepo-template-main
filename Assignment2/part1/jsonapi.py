6d5f05fc84cbfd1f2a08cdb93d962d8c562fb71a
Taeyoon Kim <taeyoonkim@Taeyoons-MacBook-Pro.local>
2023-09-19 10:38:47 -0700

    Upload Lab2 and .gitignore

---
+++ Assignment2/part1/jsonapi.py
@@ -0,0 +1,49 @@
+import json
+
+class BetterEncoder(json.JSONEncoder):
+  def default(self, obj):
+    name = type(obj).__name__
+    try:
+      encoder_func = getattr(self, f"encode_{name}")
+    except AttributeError:
+      super().default(obj)
+    else:
+      encoded = encoder_func(obj)
+      encoded["__extended_json_type__"] = name
+      return encoded 
+  
+  def encode_complex(self, obj):
+    return {"real": obj.real, "img": obj.imag}
+  
+  def encode_range(self, obj):
+    return {"start":obj.start, "stop": obj.stop, "step":obj.step}
+
+
+class BetterDecoder(json.JSONDecoder):
+
+  def __init__(self, **kwargs):
+    kwargs["object_hook"] = self.object_hook
+    super().__init__(**kwargs)
+
+  def object_hook(self, obj):
+    try:
+      name = obj.get("__extended_json_type__")
+      decoder_func = getattr(self, f"decode_{name}")
+    except (KeyError, AttributeError):
+      return obj
+    else:
+      return decoder_func(obj)
+
+  def decode_complex(self, obj):
+    return complex(obj["real"], obj["imag"])
+
+  def decode_range(self, obj):
+    return range(obj["start"], obj["stop"], obj["step"])
+
+
+
+def dumps(obj):
+  return json.dumps(obj, cls= BetterEncoder)
+
+def loads(obj):
+  return json.loads(obj, cls= BetterDecoder)
---
+++ Assignment2/part1/test_jsonapi.py
@@ -0,0 +1,14 @@
+import jsonapi
+import unittest
+
+
+class Test(unittest.TestCase):
+  def test_encode(self):
+    obj = complex(2,5)
+    
+    self.assertEqual()
+  def test_decode(self):
+
+
+if __name__ == '__main__':
+  unittest.main()
